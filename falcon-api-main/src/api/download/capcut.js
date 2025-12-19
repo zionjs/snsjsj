21eac896d57198b19524618050d8778942ff5796
Zion <rizkysaputra4224@gmail.com>
2025-12-19 08:41:15 +0700

    Add falcon-api-main/src/api/download/capcut.js from ZIP extraction

---
+++ falcon-api-main/src/api/download/capcut.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function capcutdl(url) {
+    try {
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const videoElement = $('video.player-o3g3Ag');
+        const videoSrc = videoElement.attr('src');
+        const posterSrc = videoElement.attr('poster');
+        const title = $('h1.template-title').text().trim();
+        const actionsDetail = $('p.actions-detail').text().trim();
+        const [date, uses, likes] = actionsDetail.split(',').map(item => item.trim());
+        const authorAvatar = $('span.lv-avatar-image img').attr('src');
+        const authorName = $('span.lv-avatar-image img').attr('alt');
+
+        if (!videoSrc || !posterSrc || !title || !date || !uses || !likes || !authorAvatar || !authorName) {
+            throw new Error('Beberapa elemen penting tidak ditemukan di halaman.');
+        }
+
+        return {            
+            title: title,
+            date: date,
+            pengguna: uses,
+            likes: likes,
+            author: {
+                name: authorName,
+                avatarUrl: authorAvatar
+            },
+            videoUrl: videoSrc,
+            posterUrl: posterSrc
+        };
+    } catch (error) {
+        console.error('Error fetching video details:', error.message);
+        return null;
+    }
+}
+
+module.exports = function (app) {
+app.get('/download/capcut', async (req, res) => {
+       const { url } = req.query
+        try {
+            const results = await capcutdl(url);
+            res.status(200).json({
+                status: true,
+                result: results
+            });
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+});
+}
